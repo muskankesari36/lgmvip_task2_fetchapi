9e1e2de9043b16c0a9db88759b74e121d49ff387
Muskan Kesarwani <muskankesari24@gmail.com>
2022-06-28 18:01:37 +0530

    task-2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from "react";
+import "./App.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFacebookF,faGithub,faTwitter,faInstagram} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
+
+  const [isDataFetched,setDataFetch]=useState(false);
+  const [users, setUsers] = useState([]);
+  const [isButtonClick,setBtnclick] = useState(false);
+
+  const userInfo = async () => {
+    setBtnclick(true);
+    const response = await fetch("https://reqres.in/api/users?page=1");
+
+    const jsonresponse = await response.json();
+
+    setUsers(jsonresponse.data);
+    setInterval(() => {
+      setDataFetch(true);
+    }, 1500);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="navbar">
+        <header>
+        <h2>Welcome !!</h2> 
+        <button onClick={userInfo}>Get Users</button>
+        </header>
+      </div>
+      
+{
+    isButtonClick?(
+      isDataFetched?(
+      <div className="grid">
+        {users.map(({ id, first_name, last_name, avatar, email }) => {
+        return(
+          <>
+          <div className="container">
+            <div className="card">
+            <div className='layer'></div>
+              <div className="content">
+                <div className="imgbx">
+              <img src={avatar}></img>
+            </div>
+            <hr></hr>
+            <div className="content-box">
+              <h3 className="name">{first_name} {last_name}</h3>
+              <p>{email}
+             </p>
+
+            </div>
+            </div>
+            <div className='sci'>
+
+            <a className='btn' href="#"> <FontAwesomeIcon icon={faFacebookF}></FontAwesomeIcon></a>
+            <a className='btn' href="#">  <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
+            <a className='btn' href="#">  <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon></a>
+            <a className='btn' href="#">  <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a>            </div>
+          </div>
+          </div>
+          </>
+        )})
+}
+
+      </div>
+        ):(
+
+      <div className="loading"><p>Loading.....</p></div>
+        )
+   
+    ):(
+
+<div className="section-1"></div>)
+}
+
+    </>
   );
 }
 
